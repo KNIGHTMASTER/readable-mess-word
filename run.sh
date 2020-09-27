0a0d23465aa1187a68b941c313b837ee4476e694
wissensalt <wissensalt@gmail.com>
2020-09-27 18:43:58 +0700

    check if docker is running or not

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+if [ "$(docker ps -q -f name=readable-mess-word)" ]; then
+  echo "docker container found !"
+
+  echo "stopping container"
+  docker stop readable-mess-word
+
+  echo "removing container"
+  docker rm readable-mess-word
+fi
+
+echo "running container"
 docker run -p 8081:8081 -d --name readable-mess-word readable-mess-word
